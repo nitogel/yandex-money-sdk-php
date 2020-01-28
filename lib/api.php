5863d1adf6b027f76653fa31ece219febc29c448
Kirill Legotin <nitogel@gmail.com>
2020-01-28 19:20:16 +0200

    yandex api upd

--- lib/api.php
+++ lib/api.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace YandexMoney;
 
 require_once __DIR__ . "/base.php";
@@ -6,17 +7,23 @@ require_once __DIR__ . "/base.php";
 /**
  * The Yandex.Money wallet API
  */
-class API extends BaseAPI {
+class API extends BaseAPI
+{
 
-    function __construct($access_token) {
+    function __construct($access_token)
+    {
         $this->access_token = $access_token;
     }
-    function sendAuthenticatedRequest($url, $options=array()) {
+
+    function sendAuthenticatedRequest($url, $options = [])
+    {
         $this->checkToken();
         return self::sendRequest($url, $options, $this->access_token);
     }
-    function checkToken() {
-        if($this->access_token == NULL) {
+
+    function checkToken()
+    {
+        if ($this->access_token == null) {
             throw new \Exception("obtain access_token first");
         }
     }
@@ -34,14 +41,16 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function accountInfo() {
+    function accountInfo()
+    {
         return $this->sendAuthenticatedRequest("/api/account-info");
     }
 
-    function getAuxToken($scope) {
-        return $this->sendAuthenticatedRequest("/api/token-aux", array(
+    function getAuxToken($scope)
+    {
+        return $this->sendAuthenticatedRequest("/api/token-aux", [
             "scope" => implode(" ", $scope)
-        ));
+        ]);
     }
 
     /**
@@ -58,7 +67,8 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function operationHistory($options=NULL) {
+    function operationHistory(array $options = [])
+    {
         return $this->sendAuthenticatedRequest("/api/operation-history", $options);
     }
 
@@ -76,9 +86,10 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function operationDetails($operation_id) {
+    function operationDetails($operation_id)
+    {
         return $this->sendAuthenticatedRequest("/api/operation-details",
-            array("operation_id" => $operation_id)
+            ["operation_id" => $operation_id]
         );
     }
 
@@ -96,13 +107,14 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function requestPayment($options) {
+    function requestPayment($options)
+    {
         return $this->sendAuthenticatedRequest("/api/request-payment", $options);
     }
 
     /**
      * Confirms a payment that was created using the request-payment method.
-     * 
+     *
      * @see http://api.yandex.com/money/doc/dg/reference/process-payment.xml
      * @see https://tech.yandex.ru/money/doc/dg/reference/process-payment-docpage/
      * @param array[] $options Key-value parameters collection
@@ -114,13 +126,14 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function processPayment($options) {
+    function processPayment($options)
+    {
         return $this->sendAuthenticatedRequest("/api/process-payment", $options);
     }
 
     /**
      * Accepts incoming transfer with a protection code or deferred transfer.
-     * 
+     *
      * @see http://api.yandex.com/money/doc/dg/reference/incoming-transfer-accept.xml
      * @see https://tech.yandex.ru/money/doc/dg/reference/incoming-transfer-accept-docpage/
      * @param string $operation_id
@@ -134,17 +147,18 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function incomingTransferAccept($operation_id, $protection_code=NULL) {
+    function incomingTransferAccept($operation_id, $protection_code = null)
+    {
         return $this->sendAuthenticatedRequest("/api/incoming-transfer-accept",
-            array(
-                "operation_id" => $operation_id,
+            [
+                "operation_id"    => $operation_id,
                 "protection_code" => $protection_code
-            ));
+            ]);
     }
 
     /**
      * Rejects incoming transfer with a protection code or deferred trasfer.
-     * 
+     *
      * @see http://api.yandex.com/money/doc/dg/reference/incoming-transfer-reject.xml
      * @see https://tech.yandex.ru/money/doc/dg/reference/incoming-transfer-reject-docpage/
      * @param string $operation_id
@@ -156,16 +170,17 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    function incomingTransferReject($operation_id) {
+    function incomingTransferReject($operation_id)
+    {
         return $this->sendAuthenticatedRequest("/api/incoming-transfer-reject",
-            array(
+            [
                 "operation_id" => $operation_id,
-            ));
+            ]);
     }
 
     /**
      * Builds authorization url for user's browser
-     * 
+     *
      * @see http://api.yandex.com/money/doc/dg/reference/request-access-token.xml
      * @see https://tech.yandex.ru/money/doc/dg/reference/request-access-token-docpage/
      * @param string $client_id The client_id that was assigned to the application.
@@ -173,51 +188,58 @@ class API extends BaseAPI {
      * authorization result to. Must have a string value that exactly matches
      * the redirect_uri parameter specified in the application registration
      * data. Any additional parameters required for the application can beadded
-     * at the end of the string. 
-     * @param string $scope A string of requested permissions(joined list of
+     * at the end of the string.
+     * @param array $scope A string of requested permissions(joined list of
      * strings)
      * @return response object
      */
-    public static function buildObtainTokenUrl($client_id, $redirect_uri,
-        $scope) {
+    public static function buildObtainTokenUrl(
+        string $client_id,
+        string $redirect_uri,
+        array $scope
+    ) {
         $params = sprintf(
             "client_id=%s&response_type=%s&redirect_uri=%s&scope=%s",
             $client_id, "code", $redirect_uri, implode(" ", $scope)
-            );
+        );
         return sprintf("%s/oauth/authorize?%s", Config::$MONEY_URL, $params);
     }
 
     /**
      * Exchanges temporary authorization code for an access_token.
-     * 
+     *
      * @see http://api.yandex.com/money/doc/dg/reference/obtain-access-token.xml
      * @see https://tech.yandex.ru/money/doc/dg/reference/obtain-access-token-docpage/
      * @param string $client_id The client_id that was assigned to the application.
      * @param string $code Temporary token.
      * @param string $redirect_uri URI that the OAuth server sends the
      * authorization result to. The value must exactly match the `redirect_uri` value
-     * from the previous "authorize" call. 
+     * from the previous "authorize" call.
      * @param string $client_secret A secret word for verifying the application's
      * authenticity. Specified if the service is registered with the option to
      * verify authenticity.
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    public static function getAccessToken($client_id, $code, $redirect_uri,
-            $client_secret=NULL) {
+    public static function getAccessToken(
+        $client_id,
+        $code,
+        $redirect_uri,
+        $client_secret = null
+    ) {
         $full_url = Config::$MONEY_URL . "/oauth/token";
-        return self::sendRequest($full_url, array(
-            "code" => $code,
-            "client_id" => $client_id,
-            "grant_type" => "authorization_code",
-            "redirect_uri" => $redirect_uri,
+        return self::sendRequest($full_url, [
+            "code"          => $code,
+            "client_id"     => $client_id,
+            "grant_type"    => "authorization_code",
+            "redirect_uri"  => $redirect_uri,
             "client_secret" => $client_secret
-        ));
+        ]);
     }
 
     /**
      * Revokes a token.
-     * 
+     *
      * @see http://api.yandex.com/money/doc/dg/reference/incoming-transfer-reject-xml
      * @see https://tech.yandex.ru/money/doc/dg/reference/incoming-transfer-reject-docpage/
      * @param string $token A token to be revoked
@@ -228,9 +250,10 @@ class API extends BaseAPI {
      * @throws Exceptions/ServerError If status code >= 500
      * @return response object
      */
-    public static function revokeToken($token, $revoke_all=false) {
-        return self::sendRequest("/api/revoke", array(
+    public static function revokeToken($token, $revoke_all = false)
+    {
+        return self::sendRequest("/api/revoke", [
             "revoke-all" => $revoke_all,
-        ), $token);
+        ], $token);
     }
 }
